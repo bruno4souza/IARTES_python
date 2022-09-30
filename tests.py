46776ebd311a1a0784235209b5ed78f23c55aa19
Bruno Souza <bruno_bscoari@hotmail.com>
2022-09-30 15:44:51 -0400

    Incluindo as descrições dos testes.

--- tests.py
+++ tests.py
@@ -1,22 +1,25 @@
 import funcionario as fn
 
 class TestFuncionario(object):
+    #Teste do metodo que verifica se o funcionário é maior de idade.
     def testMaiorIdade (self):
         func = fn.FuncionarioRH(25, 'nome', '121314', 100, 13)
         assert func.maiorIdade() == True
         func.set_idade(16)
         assert func.maiorIdade() == False
 
-
+    #Teste de validação da idade inserida.
     def testValidarIdade(self):
         func = fn.Funcionario()
         assert func.set_idade(25) == True
         assert func.set_idade("25") == False
     
+    #Teste de uma coleta do atributo idade
     def testPegarIdade (self):
         func = fn.FuncionarioRH(25, 'nome', '121314', 100, 13)
         assert func.get_idade() == 25
 
+    #Teste de validação de um nome inserido.
     def testValidarNome(self):
         func = fn.Funcionario()
         assert func.set_nome("Joao Paulo") == True
@@ -24,10 +27,12 @@ class TestFuncionario(object):
         assert func.set_nome(3443) == False
         assert func.set_nome(' ') == False
     
+    #Teste de uma coleta do atributo nome.
     def testPegarNome (self):
         func = fn.FuncionarioRH(25, 'nome', '121314', 100, 13)
         assert func.get_nome() == 'nome'
 
+    #Teste de validação de uma matrícula inserida.
     def testValidarMatricula(self):
         func = fn.Funcionario()
         assert func.set_matricula('N1420B') == True
@@ -35,50 +40,59 @@ class TestFuncionario(object):
         assert func.set_matricula(344334) == False
         assert func.set_matricula(' ') == False
     
+    #Teste de uma coleta do atributo matrícula.
     def testPegarMatricula (self):
         func = fn.FuncionarioRH(25, 'nome', '121314', 100, 13)
         assert func.get_matricula() == '121314'
 
+    #Teste de validação do método de contratação de funcionario.
     def testPegarFuncionario (self):
         func = fn.Funcionario()
         assert func.pegar_funcionario() == 'Funcionario Contratado'
     
+    #Teste que verifica a meta de contratação do funcionário de RH.
     def testMetaContratacao(self):
         func = fn.FuncionarioRH()
         assert func.set_meta_contratacao(100) == True
         assert func.set_meta_contratacao("25") == False
     
+    #Teste de uma inserção e coleta do atributo meta de contratação.
     def testPegaMetaContratacao(self):
         func = fn.FuncionarioRH()
         func.set_meta_contratacao(100)
         assert func.get_meta_contratacao() == 100
 
+    #Teste que valida uma inserção incorreta do atributo qtsd contratação.
     def testQtdContratacao(self):
         func = fn.FuncionarioRH()
         assert func.set_qtd_contratacao(100) == True
         assert func.set_qtd_contratacao("25") == False
     
+    #Teste de uma inserção e coleta do atributo qtd contratação.
     def testPegaQtdContratacao(self):
         func = fn.FuncionarioRH()
         func.set_qtd_contratacao(45)
         assert func.get_qtd_contratacao() == 45
     
+    #Teste que verifica a contratação de uma novo funcionário usando o metodo recrutar funcionário.
     def testRecrutarFuncionario (self):
         func = fn.FuncionarioRH()
         func.set_qtd_contratacao(45)
         assert func.recrutar_funcionario() == 'Novo Funcionario Recrutado'
         assert func.get_qtd_contratacao() == 46
     
-
+    #Teste que verifica o método que permite ao funcionário de RH palestrar.
     def testpalestrar (self):
         func = fn.FuncionarioRH()
         func.palestrar()
         assert  func.palestrar() == "Recrutador esta palestrando"
 
+    #Teste que verifica a utilização da senha de rede do funcionário de TI.
     def testSenhaRede(self):
         func = fn.FuncionarioTI()
         assert func.set_senha_rede("P@ssw0rd123") == True
 
+    #Teste que verifica uma inserção e consumo do atributo de senha de rede.
     def testPegarSenhaRede(self):
         func = fn.FuncionarioTI()
         func.set_senha_rede("P@ssw0rd123")
