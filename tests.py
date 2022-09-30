27ebc5ca55a4287ea6475376d87fe49297e32926
Bruno Souza <bruno.bscoari@gmail.com>
2022-09-30 09:24:03 -0300

    Update tests.py

--- tests.py
+++ tests.py
@@ -3,5 +3,4 @@ import funcionario as fn
 class TestFuncionario(object):
     def testMaiorIdade (self):
         func = fn.FuncionarioRH(25, 'nome', '121314', 100, 13)
-        assert func.maiorIdade(func.get_idade()) == True
-#tESTE
+        assert func.maiorIdade() == True
