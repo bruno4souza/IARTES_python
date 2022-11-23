7f01c2f776ee2e37f9b23fcc73b086892ee31aed
Joao Paulo Reis Marques <joaopaulo.rm7@gmail.com>
2022-11-23 18:15:19 -0400

    a

--- tests.py
+++ tests.py
@@ -4,4 +4,3 @@ class TestFuncionario(object):
     def testMaiorIdade (self):
         func = fn.FuncionarioRH(25, 'nome', '121314', 100, 13)
         assert func.maiorIdade(func.get_idade()) == True
-#tESTE
